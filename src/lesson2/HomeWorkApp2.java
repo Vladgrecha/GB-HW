24db8cb644129151cca65be484154eddb782e80d
Vladislav Grecha <v.grecha@frostgate.studio>
2021-12-24 13:55:08 +0700

    HW-2: did ex-2

--- src/lesson2/HomeWorkApp2.java
+++ src/lesson2/HomeWorkApp2.java
@@ -4,11 +4,14 @@ public class HomeWorkApp2 {
     public static void main(String[] args) {
         int a = 11;
         int b = -1;
+
         if (checkSum (a, b)) {
             System.out.println("Сумма лежит в промежутке 10-20");
         } else {
             System.out.println("Сумма лежит за пределами 10-20");
         }
+
+        
         }
 
         //Задани-1
