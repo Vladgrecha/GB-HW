a54ad1c557e28afe1efe1edc0d39448fa0aee539
Vladislav Grecha <v.grecha@frostgate.studio>
2021-12-24 14:14:30 +0700

    HW-2: did ex-3

--- src/lesson2/HomeWorkApp2.java
+++ src/lesson2/HomeWorkApp2.java
@@ -4,21 +4,29 @@ public class HomeWorkApp2 {
     public static void main(String[] args) {
         int a = 11;
         int b = -1;
-        int value = 0;
+        int number = 0;
+        int value = -1;
 
+        System.out.println("************Ex-1************");
         if (checkSum (a, b)) {
             System.out.println("Сумма лежит в промежутке 10-20");
         } else {
             System.out.println("Сумма лежит за пределами 10-20");
         }
 
-        if (isNumberPositive(value)) {
+        System.out.println("************Ex-2************");
+        if (isNumberPositive(number)) {
             System.out.println ("Number is positive");
         } else {
             System.out.println ("Number is negative");
         }
 
-
+        System.out.println("************Ex-3************");
+        if (isNumberNegative(value)) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is positive");
+        }
         }
 
         //Задани-1
@@ -30,4 +38,12 @@ public class HomeWorkApp2 {
     public static boolean isNumberPositive (int number) {
         return number >= 0;
     }
+
+        //Задание-3 *(0 по аналогии со вторым задание тоже учитываю как положительное число)*
+    public static boolean isNumberNegative (int value) {
+        return value < 0;
+    }
+
+        //Задание-4
+    
     }
